1218f63f2fa60557fc91974815a3a7fc3c71d93d
erdo <kopuskopecik@gmail.com>
2020-12-22 18:39:51 +0200

    Sending Token request from client

--- client/src/components/modals/Login.js
+++ client/src/components/modals/Login.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import axios from "axios";
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 
-const sendForm = (e)=> {
+const sendForm = async (e)=> {
   e.preventDefault();
   console.log(e.target[0].value);
   console.log(e.target[1].value);
+  const data =  {
+    email: e.target[0].value,
+    password: e.target[1].value
+  }
+  const response = await axios.post("/api/login", data);
+
+  console.log(response.data.token);
 }
 
 
 const Login = (props) => {
   return (
-    <Form inline onSubmit= {sendForm}>
+    <Form onSubmit= {sendForm}>
       <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
         <Label for="exampleEmail" className="mr-sm-2">Email</Label>
         <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
@@ -20,7 +28,7 @@ const Login = (props) => {
         <Label for="examplePassword" className="mr-sm-2">Password</Label>
         <Input type="password" name="password" id="examplePassword" placeholder="don't tell!" />
       </FormGroup>
-      <Button>Submit</Button>
+      <Button className = "mt-2 btn-block btn-small btn-success">Submit</Button>
     </Form>
   );
 }
